eb3e2b0642f1ffaaa673958b4ab6a6db0a5d58c6
Diabeu <scibor.rudnicki@ster-project.pl>
2017-05-23 17:28:03 +0200

    chore: add plugins to have component external template functionality

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,9 @@
-
-import nodeResolve  from 'rollup-plugin-node-resolve';
-import commonjs     from 'rollup-plugin-commonjs';
-import uglify       from 'rollup-plugin-uglify';
+import angular from 'rollup-plugin-angular';
+import commonjs from 'rollup-plugin-commonjs';
+import html from 'rollup-plugin-html';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import typescript from 'rollup-plugin-typescript';
+import uglify from 'rollup-plugin-uglify';
 
 export default {
   entry: 'dist/index.js',
@@ -12,17 +14,42 @@ export default {
   moduleName: '@ngx-prism/core',
   onwarn,
   plugins: [
-      commonjs({
-          namedExports: {
-            'node_modules/rxjs/**': [ 'named' ]
-          }
-      }),
-      nodeResolve({
-        jsnext: true,
-        module: true,
-        extensions: [ '.js', '.json', 'html']
-      }),
-      uglify()  
+    angular(),
+    commonjs({
+      namedExports: {
+        'node_modules/rxjs/**': ['named']
+      }
+    }),
+    html({
+      include: '**/*.html',
+      htmlMinifierOptions: {
+        caseSensitive: true // need to do not lower letter
+      }
+    }),
+    nodeResolve({
+      // use "es2015" field for ES2015 modules with ES2015 code,
+	    // if possible
+      es2015: true, // Default: false
+
+      // use "module" field for ES2015 modules with ES5 code,
+      // if possible
+      module: true, // Default: true
+
+      // use "jsnext:main" if possible
+      // – see https://github.com/rollup/rollup/wiki/jsnext:main
+      jsnext: true,  // Default: false
+
+      // use "main" field or index.js, even if it's not an ES6 module
+      // (needs to be converted from CommonJS to ES6
+      // – see https://github.com/rollup/rollup-plugin-commonjs
+      main: true,  // Default: true
+
+      extensions: [ '.js', '.json', 'html']
+    }),
+    typescript({
+      typescript: require('./node_modules/typescript')
+    }),
+    uglify()  
   ],
   globals: {
     '@angular/core': 'ng.core'
