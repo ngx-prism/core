794f978ff81b8dbb6e9749118a8189f1242c8222
Diabeu <scibor.rudnicki@ster-project.pl>
2017-05-23 17:26:36 +0200

    chore: add rollup plugins to karma to have working external template functionality

--- karma.conf.js
+++ karma.conf.js
@@ -1,9 +1,10 @@
 // Karma configuration
 // Generated on Mon Mar 20 2017 15:06:42 GMT+0100 (CET)
 
-const nodeResolve = require('rollup-plugin-node-resolve');
-const commonjs = require('rollup-plugin-commonjs');
 const angular = require('rollup-plugin-angular');
+const commonjs = require('rollup-plugin-commonjs');
+const html = require('rollup-plugin-html');
+const nodeResolve = require('rollup-plugin-node-resolve');
 const typescript = require('rollup-plugin-typescript');
 
 module.exports = function(config) {
@@ -50,16 +51,41 @@ module.exports = function(config) {
       sourceMap: 'inline',
       // rollup settings. See Rollup documentation
       plugins: [
-        angular(),
+        // angular(),
+        commonjs({
+          namedExports: {
+            'node_modules/rxjs/**': ['named']
+          }
+        }),
+        html({
+          include: '**/*.html',
+          htmlMinifierOptions: {
+            caseSensitive: true // need to do not lower letter
+          }
+        }),
+        nodeResolve({
+          // use "es2015" field for ES2015 modules with ES2015 code,
+          // if possible
+          es2015: true, // Default: false
+
+          // use "module" field for ES2015 modules with ES5 code,
+          // if possible
+          module: true, // Default: true
+
+          // use "jsnext:main" if possible
+          // – see https://github.com/rollup/rollup/wiki/jsnext:main
+          jsnext: true,  // Default: false
+
+          // use "main" field or index.js, even if it's not an ES6 module
+          // (needs to be converted from CommonJS to ES6
+          // – see https://github.com/rollup/rollup-plugin-commonjs
+          main: true,  // Default: true
+
+          extensions: [ '.js', '.json', 'html']
+        }),
         typescript({
           typescript: require('./node_modules/typescript')
         }),
-        commonjs(),
-        nodeResolve({
-          jsnext: true,
-          browser: true,
-          extensions: [ '.js', '.json', 'html']
-        })
       ],
     },
 
