77fe2ebab2dfc0c845c46c66757a7f4f6c1b2519
Diabeu <scibor.rudnicki@ster-project.pl>
2017-09-14 16:49:18 +0200

    feat(PrismClass): add abstract class to extends component

---
+++ src/prism.class.ts
@@ -0,0 +1,96 @@
+/// <reference path="./../typings/index.d.ts" />
+// external
+import {
+  ElementRef,
+  Input,
+  Injectable,
+  ViewChild
+} from '@angular/core';
+import Prism from 'prismjs';
+
+// internal
+import { CallbackType } from './prism.type';
+
+/**
+ * @export
+ * @abstract
+ * @class PrismClass
+ */
+@Injectable()
+export abstract class PrismClass {
+  changed = false;
+
+  /**
+   * Whether to use Web Workers to improve performance and avoid blocking the UI when highlighting very large chunks of code.
+   * False by default (why? - http://prismjs.com/faq.html#why-is-asynchronous-highlighting-disabled-by-default).
+   */
+  @Input('async') public async = false;
+
+  /**
+   * An optional callback to be invoked after the highlighting is done.
+   * Mostly useful when async is true, since in that case, the highlighting is done asynchronously.
+   * @memberof PrismComponent
+   */
+  @Input('callback') public callback?: CallbackType | undefined = undefined;
+
+  /**
+   * A string with the code to be highlighted.
+   * @type {string}
+   * @memberof PrismComponent
+   */
+  _code: string;
+  @Input('code') set code(value: string) {
+    this._code = value;
+  }
+  get code(): string {
+    return this._code;
+  }
+
+  /**
+   * "The element containing the code. It must have a class of language-xxxx to be processed, where xxxx is a valid language identifier."
+   * @type {ElementRef}
+   * @memberof PrismComponent
+   */
+  @ViewChild('codeElementRef') protected codeElementRef: ElementRef;
+
+  /**
+   * Valid language identifier.
+   * @type {string}
+   * @memberof PrismComponent
+   */
+  @Input('language') public language: string;
+
+  /**
+   * Use highlight method depends on recevied boolean parameter `changed`.
+   * @param {boolean} [changed=false]
+   * @memberof PrismClass
+   */
+  highlight(changed = false): void {
+    if (this.changed === changed) {
+      if (this.codeElementRef) {
+        if (this.code) {
+          this.highlightCode();
+        } else {
+          this.highlightElement();
+        }
+        this.changed = false;
+      }
+    }
+  }
+
+  /**
+   * Use `prismjs` to highlight string in property `code` and assign to nativeElement.
+   * @memberof PrismClass
+   */
+  highlightCode(): void {
+    this.codeElementRef.nativeElement.innerHTML = Prism.highlight(this.code, Prism.languages[this.language]);
+  }
+
+  /**
+   * Use `prismjs` to highlight code in nativeElement.
+   * @memberof PrismClass
+   */
+  highlightElement(): void {
+    Prism.highlightElement(this.codeElementRef.nativeElement, this.async, this.callback);
+  }
+}
