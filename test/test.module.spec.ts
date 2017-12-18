ec589d439d88bd17dcd62173a16c75ef1d366ed9
Diabeu <scibor.rudnicki@ster-project.pl>
2017-12-18 10:21:45 +0100

    test: new ngx-prism tag

--- test/test.module.spec.ts
+++ test/test.module.spec.ts
@@ -40,8 +40,8 @@ describe('TestComponent', () => {
     expect(fixture).toBeDefined();
     expect(comp).toBeTruthy();
   }));
-  it('should have `prism-highlight` html tag', async(() => {
-    expect(nativeElement.querySelector('prism-highlight')).toBeTruthy();
+  it('should have `ngx-prism` html tag', async(() => {
+    expect(nativeElement.querySelector('ngx-prism')).toBeTruthy();
   }));
   it('should have `language` property defined', async(() => {
     expect(comp.language).toBe('html');
@@ -50,33 +50,33 @@ describe('TestComponent', () => {
   // ng-content
   it('it should have `ng-content` highlighted and interpolated.', () => {
     fixture.detectChanges();
-    expect(nativeElement.querySelector('prism-highlight[id="ng-content"]').innerHTML).toContain(comp.text);
+    expect(nativeElement.querySelector('ngx-prism[id="ng-content"]').innerHTML).toContain(comp.text);
   });
   it('should have component `ng-content` changed.', async(() => {
     comp.code.content = `<p align="left">{{interpolated}}</p>`;
     fixture.detectChanges();
-    expect(nativeElement.querySelector('prism-highlight[id="ng-content"]').innerHTML).toContain(`left`);
+    expect(nativeElement.querySelector('ngx-prism[id="ng-content"]').innerHTML).toContain(`left`);
   }));
 
   // Test `code` property.
   it('should have component property `code` with css working.', async(() => {
     fixture.detectChanges();
-    expect(nativeElement.querySelector('prism-highlight[id="code-css"]').innerText).toContain(`text-align`);
+    expect(nativeElement.querySelector('ngx-prism[id="code-css"]').innerText).toContain(`text-align`);
   }));
   it('should have component property `code` with html working.', async(() => {
     fixture.detectChanges();
-    expect(nativeElement.querySelector('prism-highlight[id="code-html"]').innerText).toContain(`My p`);
+    expect(nativeElement.querySelector('ngx-prism[id="code-html"]').innerText).toContain(`My p`);
   }));
   it('should have component property `code` with html and interpolation working.', async(() => {
     fixture.detectChanges();
-    expect(nativeElement.querySelector('prism-highlight[id="code-interpolation"]').innerText).toContain(comp.text);
+    expect(nativeElement.querySelector('ngx-prism[id="code-interpolation"]').innerText).toContain(comp.text);
   }));
   it('should have component property `code` with html and interpolation working.', async(() => {
     comp.language = 'javascript';
     comp.code.html = `<p align="center" style="">My p {{language}}</p>`;
     fixture.detectChanges();
     expect(nativeElement.querySelector(`code[class~="language-${comp.language}"]`)).toBeTruthy();
-    expect(nativeElement.querySelector('prism-highlight[id~="code-interpolation"]').innerText).toContain(`My p ${comp.language}`);
+    expect(nativeElement.querySelector('ngx-prism[id~="code-interpolation"]').innerText).toContain(`My p ${comp.language}`);
   }));
 
 });
