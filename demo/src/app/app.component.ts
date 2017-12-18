1bfd44e26dfb7334a200e5d1016b8bacdf4378af
Diabeu <scibor.rudnicki@ster-project.pl>
2017-12-18 14:09:20 +0100

    refactor(AppComponent): different approach

--- demo/src/app/app.component.ts
+++ demo/src/app/app.component.ts
@@ -1,120 +1,233 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormArray, FormGroup, FormBuilder } from '@angular/forms';
+
+import { memoize } from 'lodash-decorators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  @ViewChild('textarea') textarea: ElementRef;
+export class AppComponent implements OnInit {
 
   async = false;
-  code = `
-  /*
-    ng-content does not change.
-    interpolation title: '{{title}}'
-  */
-  .item {
-    text-align: center;
-    language: {{language}};
-  }`;
-  css: string;
+  prism: Array<any>;
+  examples = [
+    {
+      title: `Property [code] example`,
+      debug: 'Add something to debug',
+      html: `
+      <ngx-prism
+        [async]="prism[0]?.async"
+        [hooks]="prism[0]?.hooks"
+        [language]="prism[0].language"
+        [interpolation]="prism[0]?.interpolation"
+        [code]="prism[0]?.code"
+        [callback]="prism[0]?.callback"
+      ></ngx-prism>`,
+      ts: `
+      [
+        {
+          async: false,
+          hooks: {
+            'before-sanity-check': (env) => { console.log('before-sanity-check', env); },
+            'before-highlight': (env) => { console.log('before', env); },
+            'after-highlight': (env) => { console.log('after', env); },
+            'complete': (env) => { console.log('complete', env); },
+            'before-insert': (env) => { console.log('before-insert', env); }
+          },
+          callback: () => {
+            console.log('callback() zero');
+          },
+          code:
+  \`    /*
+        ng-content does not change.
+        interpolation title: '{{title}}'
+      */
+      .item {
+        text-align: center;
+        language: {{language}};
+      }\`,
+          interpolation: {
+            language: 'css',
+            title: this.examples[0].title
+          },
+          language: 'css'
+        }
+      ]`,
+      css: ``
+    },
+    {
+      title: `<ng-content></ng-content> example`,
+      debug: 'Add something ',
+      html: `
+      <ngx-prism
+        [async]="prism[0]?.async"
+        [hooks]="prism[0]?.hooks"
+        [language]="prism[0].language"
+        [interpolation]="prism[0]?.interpolation"
+        [callback]="prism[0]?.callback"
+      >{{prism[0]?.ngContent}}</ngx-prism>`,
+      ts: `
+      [
+        {
+          async: false,
+          hooks: {
+            'before-sanity-check': (env) => { console.log('before-sanity-check', env); },
+            'before-highlight': (env) => { console.log('before', env); },
+            'after-highlight': (env) => { console.log('after', env); },
+            'complete': (env) => { console.log('complete', env); },
+            'before-insert': (env) => { console.log('before-insert', env); }
+          },
+          code: \`\`,
+          callback: () => {
+            console.log('callback() one');
+          },
+          ngContent:
+  \`    <p align="center">
+          This is example <strong>html code</strong>.
+      </p>\`,
+          interpolation: {
+            language: 'html',
+            title: this.examples[1].title
+          },
+          language: 'html'
+        }
+      ]`,
+      css: ``
+    }
+  ];
+
   form: FormGroup;
-  hooks = {
-    'before-sanity-check': (env) => { console.log(`before-sanity-check`, env); },
-    'before-highlight': (env) => { console.log(`before-highlight`, env); },
-    'after-highlight': (env) => { console.log(`after-highlight`, env); },
-    'complete': (env) => { console.log(`complete`, env); },
-    'before-insert': (env) => { console.log(`before-insert`, env); }
-  };
-  html = `
-  <prism-highlight
-    [async]="async"
-    [hooks]="hooks"
-    [language]="language"
-    [interpolation]="interpolate"
-    [code]="code"
-    [callback]="callback"
-  ></prism-highlight>
-  <prism-highlight
-    [async]="async"
-    [language]="language"
-    [interpolation]="interpolate"
-  >{{code}}</prism-highlight>`;
-  language = 'css';
   languages = [
     'css', 'javascript', 'html', `markup`
   ];
   payload: string;
-  selectedLanguage = 'css';
-  title = '@ngx-prism/core example';
-  ts = `
-  async = ${this.async};
-  language = '${this.language}';
-  code = \`${this.code}\`;
-  callback = () => {
-    console.log('callback');
-  };
-  interpolate = {
-    language: this.language,
-    title: this.title
-  };`;
 
-  interpolate = {
-    language: this.language,
-    title: this.title
-  };
+  constructor(public fb: FormBuilder) {}
 
-  constructor(fb: FormBuilder) {
-    this.form = fb.group({
-      async: this.async,
-      code: this.code,
-      html: this.html,
-      language: this.language
-    });
-  }
-  callback = () => {
-    console.log('callback');
-  }
-  update($event) {
-    console.log(`update()`, $event);
-    Object.assign(this, {
-      [$event.srcElement.name]: $event.srcElement.value
-    });
-    this.setTs();
-  }
-  setTs() {
-    this.ts = `
-  async = ${this.async};
-  language = '${this.language}';
-  code = \`${this.code}\`;
-  callback = () => {
-    console.log('callback');
-  };
-  interpolate = {
-    language: this.language,
-    title: this.title
-  };`;
-  }
-  setAsync(value) {
-    console.log(`setAsync`, value);
-    this.async = (value === true) ? false : true;
-    this.setTs();
-  }
-  setLanguage(value) {
-    console.log(`setLanguage`, value);
-    this.language = value;
-    this.setTs();
-  }
-  submit(form) {
-    console.log(form);
-    this.payload = JSON.stringify(form.value);
-    for (const key in form.value) {
-      if (key) {
-        this[key] = form.value[key];
+  ngOnInit() {
+    this.prism = [
+      {
+        __properties: {
+          async: true,
+          callback: true,
+          code: true,
+          hooks: true,
+          language: true,
+          interpolation: true
+        },
+        async: true,
+        hooks: {
+          'before-sanity-check': (env) => { console.log('before-sanity-check', env); },
+          'before-highlight': (env) => { console.log('before', env); },
+          'after-highlight': (env) => { console.log('after', env); },
+          'complete': (env) => { console.log('complete', env); },
+          'before-insert': (env) => { console.log('before-insert', env); }
+        },
+        callback: () => {
+          console.log('callback() zero');
+        },
+        code:
+`    /*
+      ng-content does not change.
+      interpolation title: '{{title}}'
+    */
+    .item {
+      text-align: center;
+      language: {{language}};
+    }`,
+        interpolation: {
+          language: 'css',
+          title: this.examples[0].title
+        },
+        language: 'css'
+      },
+      {
+        __properties: {
+          async: true,
+          callback: true,
+          code: false,
+          hooks: true,
+          language: true,
+          interpolation: true
+        },
+        async: false,
+        hooks: {
+          'before-sanity-check': (env) => { console.log('before-sanity-check', env); },
+          'before-highlight': (env) => { console.log('before', env); },
+          'after-highlight': (env) => { console.log('after', env); },
+          'complete': (env) => { console.log('complete', env); },
+          'before-insert': (env) => { console.log('before-insert', env); }
+        },
+        code: ``,
+        callback: () => {
+          console.log('callback() one');
+        },
+        ngContent:
+`    <p align="center">
+        This is example <strong>html code</strong>.
+    </p>`,
+        interpolation: {
+          language: 'html',
+          title: this.examples[1].title
+        },
+        language: 'html'
       }
-    }
+    ];
+
+    // FORM
+    this.form = this.fb.group({
+      prism: this.createForm()
+    });
+  }
+
+
+  update(property) {
+    this[property] = Object.assign({}, this[property]);
+  }
+
+  keyup($event) {
+    console.log(`keyup: update `, $event.srcElement.name);
+
+    // this is the best way to update json data
+    this[$event.srcElement.name] = this.form.value[$event.srcElement.name];
+
+    // This won't work with set
+    // this.address[$event.srcElement.name] = $event.srcElement.value;
+
+    // This will update address city value
+    // this.address = Object.assign({}, { city: $event.srcElement.value });
+  }
+
+  /**
+   * @returns
+   * @memberof AppComponent
+   */
+  createForm() {
+    const arr = [];
+    this.prism.forEach((prism, key) => {
+      const g = {};
+      Object.keys(prism).forEach((v) => {
+        if (v === '__properties') {
+          g[v] = this.fb.group(this.prism[key][v]);
+        } else {
+          g[v] = this.prism[key][v];
+        }
+      });
+      arr.push(this.fb.group(g));
+    });
+    return this.fb.array(arr);
+  }
+
+  /**
+   * @param {any} form
+   * @returns
+   * @memberof AppComponent
+   */
+  submit(form) {
+    this.payload = JSON.stringify(form.value);
+    console.log(this.payload);
     return false;
   }
 }
